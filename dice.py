bfd10213d45edf733bb4e830055f05401c63bcc2
Joseph Cueto <70298321+jcueto1998@users.noreply.github.com>
2020-09-13 20:28:49 -0700

    Add files via upload

---
+++ dice.py
@@ -0,0 +1,12 @@
+from random import choices
+ntrials=15000
+player1wins=0
+ndiceplayer1=3
+ndiceplayer2=2
+for i in range(ntrials):
+    dice=choice(range(1,7), k=ndice
+    .sort(reverse=True)
+    total=total+dice[0]+dice[1]+dice[3]
+    total=total+dice[0]+dice[1]
+print("Average roll=", player1wins/ntrials)
+
